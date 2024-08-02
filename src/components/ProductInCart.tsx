7783a7f7a4b156545a5daccb06e81fa56bf4b498
RASZZZKING <farras.akhirio.ramadhan.204@gmail.com>
2024-08-02 11:47:21 +0700

    war is over

--- src/components/ProductInCart.tsx
+++ src/components/ProductInCart.tsx
@@ -16,6 +16,9 @@ const ProdcutInCart: FunctionComponent<ProdcutInCartProps> = ({
   const [totalBiaya, setTotalBiayaState] = useState(0);
   const [alertMuncul, setAlertMunculState] = useState(false);
   const [varr, setVarr] = useState(false);
+  const [sudahDihapus, setSudahDihapus] = useState<boolean[]>(
+    new Array(theProduct.data.slice(0, 6).length).fill(false)
+  );
 
   useEffect(() => {
     let total = 0;
@@ -56,110 +59,112 @@ const ProdcutInCart: FunctionComponent<ProdcutInCartProps> = ({
     });
   };
 
+  const handleCloose = () => {
+    setAlertMunculState(false);
+  };
+
+  const handleBeneran = (index: number) => {
+    handleCloose();
+    setVarr(true);
+    setSudahDihapus((prev) => {
+      const newStatus = [...prev];
+      newStatus[index] = true;
+      return newStatus;
+    });
+    setTimeout(() => {
+      setVarr(false);
+    }, 3000);
+  };
+
   return (
     <>
       <div className="flex flex-col gap-4 rounded-xl overflow-y-auto hidden-scrollbar">
-        {theProduct.data.slice(0, 6).map((cb, i) => {
-          const [sudahDihapus,setSudahDihapus] = useState(false)
-          const handleCloose = () => {
-            setAlertMunculState(false);
-          };
-          const handleBeneran = () => {
-            handleCloose();
-            setVarr(true);
-            setSudahDihapus(true)
-            setTimeout(() => {
-              setVarr(false);
-            }, 3000);
-          };
-
-          return (
-            <div key={i}  className={` ${sudahDihapus ?"hidden":"flex"} gap-4 items-center`}>
-              <img
-                src={cb.imageUrl}
-                className="aspect-square h-20 object-cover shadow-lg rounded-xl"
-              />
-              <div className="grow flex flex-col gap-1">
-                <div className="flex flex-col">
-                  <p className="text-base font-semibold">{cb.title}</p>
-                  <p className="text-xs font-semibold opacity-50">
-                    Size: {cb.size[0]} - Color: {cb.colors[0].name}
-                  </p>
-                </div>
-                <div className="flex items-center">
-                  <p className="text-xs font-semibold opacity-50">$</p>
-                  <p className="font-semibold text-base">
-                    {cb.price * productQuantities[i]}
-                  </p>
-                </div>
-              </div>
-              <div className="flex gap-4 items-center">
-                <Minus
-                  size={18}
-                  weight="bold"
-                  onClick={() => ubahBiayaMinus(i, cb.price)}
-                  className={`${productQuantities[i] === 1 && "disabled"}`}
-                />
-                <p className="font-semibold text-base">
-                  {productQuantities[i]}
+        {theProduct.data.slice(0, 6).map((cb, i) => (
+          <div key={i} className={`${sudahDihapus[i] ? "hidden" : "flex"} gap-4 items-center`}>
+            <img
+              src={cb.imageUrl}
+              className="aspect-square h-20 object-cover shadow-lg rounded-xl"
+            />
+            <div className="grow flex flex-col gap-1">
+              <div className="flex flex-col">
+                <p className="text-base font-semibold">{cb.title}</p>
+                <p className="text-xs font-semibold opacity-50">
+                  Size: {cb.size[0]} - Color: {cb.colors[0].name}
                 </p>
-                <Plus
-                  size={18}
-                  weight="bold"
-                  onClick={() => ubahBiayaPlus(i, cb.price)}
-                  className="text-color-main"
-                />
               </div>
-              <div className={`${varr ? "alert-show" : "alert-hide"}`}>
-                <div role="alert" className="alert alert-success">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span>{cb.title} has been deleted!</span>
-                </div>
+              <div className="flex items-center">
+                <p className="text-xs font-semibold opacity-50">$</p>
+                <p className="font-semibold text-base">
+                  {cb.price * productQuantities[i]}
+                </p>
               </div>
-              <div className={`${alertMuncul ? "alert-show" : "alert-hide"}`}>
-                <div role="alert" className="alert alert-warning">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24"
+            </div>
+            <div className="flex gap-4 items-center">
+              <Minus
+                size={18}
+                weight="bold"
+                onClick={() => ubahBiayaMinus(i, cb.price)}
+                className={`${productQuantities[i] === 1 && "disabled"}`}
+              />
+              <p className="font-semibold text-base">
+                {productQuantities[i]}
+              </p>
+              <Plus
+                size={18}
+                weight="bold"
+                onClick={() => ubahBiayaPlus(i, cb.price)}
+                className="text-color-main"
+              />
+            </div>
+            <div className={`${varr ? "alert-show" : "alert-hide"}`}>
+              <div role="alert" className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{cb.title} has been deleted!</span>
+              </div>
+            </div>
+            <div className={`${alertMuncul ? "alert-show" : "alert-hide"}`}>
+              <div role="alert" className="alert alert-warning">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+                <span>Warning: Do you sure to delete this product from your cart?</span>
+                <div>
+                  <button onClick={handleCloose} className="btn btn-xs me-2">
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => handleBeneran(i)}
+                    className="btn btn-xs btn-primary btn-error"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                    />
-                  </svg>
-                  <span>Warning: Do you sure to delete this product from your cart?</span>
-                  <div>
-                    <button onClick={handleCloose} className="btn btn-xs me-2">
-                      Cancel
-                    </button>
-                    <button
-                      onClick={handleBeneran}
-                      className="btn btn-xs btn-primary btn-error"
-                    >
-                      Yes
-                    </button>
-                  </div>
+                    Yes
+                  </button>
                 </div>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </>
   );
